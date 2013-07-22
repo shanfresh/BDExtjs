4a23d4a84ae86d0dd1341062a822383a05887297
shanjixi <networktest1@126.com>
2013-07-22 15:38:44 +0800

    Add UserSession To AppopControl

--- application/controllers/AppopControl.php
+++ application/controllers/AppopControl.php
@@ -2,6 +2,8 @@
 class AppopControl extends CI_Controller{
 	function  __construct(){
 		parent::__construct();
+		$this->load->library('session');
+	 	$this->session->set_userdata('UserName', 'Shanjixi');
 	}
 	function load(){
 		
