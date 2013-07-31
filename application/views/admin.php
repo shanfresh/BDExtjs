635d4cabc4fbb401de287058a109159d97c15441
shanfresh <networktest1@126.com>
2013-07-31 17:34:09 +0800

    修改admin,使得DataPick宽度正确

--- application/views/admin.php
+++ application/views/admin.php
@@ -3,15 +3,24 @@
 <!-- Auto Generated with Sencha Architect -->
 <!-- Modifications to this file will be overwritten. -->
 <html>
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>BVC管理员页面</title>
     <script src="http://cdn.sencha.com/ext/gpl/4.2.1/ext-all-debug.js"></script>
     <link rel="stylesheet" href="http://extjs.cachefly.net/ext-4.1.1-gpl/resources/css/ext-all.css">
     <script type="text/javascript" src="admin.js"></script>
+	<style type="text/css">
+		.x-datepicker{
+			width:300px
+		}
+	</style>
 </head>
 
+
+
 <body>
+
 <h3 align="right">欢迎您:<?php echo phpCAS::getUser(); ?>  &nbsp&nbsp<a href="?logout=">登出</a></h3>
 <div id="adminpanel"></div>
 </body>
